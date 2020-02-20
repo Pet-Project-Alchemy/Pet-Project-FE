a038827cfb1a2453bbeb4ba8c171b081999e559a
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-20 10:26:22 -0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// eslint-disable-next-line
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    publicPath: '/'
+  },
+  devServer: {
+    port: 7890,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin(),
+    new Dotenv({
+      systemvars: true
+    }),
+    new CopyPlugin([{ from: 'public' }])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 }
+        }
+      }
+    ]
+  }
+};
