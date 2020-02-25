82ae18f55c1dbae62b720ecb150b8ae60ce854a9
Eve Stockstill <51726275+evestockstill@users.noreply.github.com>
2020-02-25 15:52:22 -0800

    Post to search (#10)
    * stashing
    
    * route to redirect to zip

--- src/service/fetchUsersViaZipcode.js
+++ src/service/fetchUsersViaZipcode.js
@@ -1,6 +1,6 @@
 export const getUsersViaZipcode = (zipcode) => {
   return fetch(
-    `https://pet-project-be-staging.herokuapp.com/api/v1/auth/${zipcode}`,
+    `https://pet-project-be-staging.herokuapp.com/api/v1/auth/zipcode/${zipcode}`,
     {
       method: 'GET',
       
