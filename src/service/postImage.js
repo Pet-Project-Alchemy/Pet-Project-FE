5d2c733a0f17a40ec451feea2d60adee36704114
Zak Hyslin <rapscalan@gmail.com>
2020-02-26 10:16:00 -0800

    update url

--- src/service/postImage.js
+++ src/service/postImage.js
@@ -1,7 +1,6 @@
 
 export const fileUpload = (data) => {
-  return fetch('http://localhost:7891/api/v1/auth/signup', {
-  //return fetch('https://pet-project-be-staging.herokuapp.com/api/v1/auth/signup', {
+  return fetch('https://pet-project-be-staging.herokuapp.com/api/v1/auth/signup', {
     method: 'POST',
     body: data
   })
