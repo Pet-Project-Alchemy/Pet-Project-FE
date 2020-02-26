b652f80f4f36f842dad7ddea2dad2bc4bab8bb99
Zak Hyslin <rapscalan@gmail.com>
2020-02-26 08:48:47 -0800

    postImage replaces fetchSignup?

--- src/service/postImage.js
+++ src/service/postImage.js
@@ -1,8 +1,7 @@
 
 export const fileUpload = (data) => {
-  //console.log('in postImage.js data: ', data);
-  //return fetch('http://localhost:7891/api/v1/images', {
-  return fetch('http://localhost:7891/api/v1/auth/signup', {
+  //return fetch('http://localhost:7891/api/v1/auth/signup', {
+  return fetch('https://cors-anywhere.herokuapp.com/https://pet-project-be-staging.herokuapp.com/api/v1/auth/signup', {
     method: 'POST',
     body: data
   })
