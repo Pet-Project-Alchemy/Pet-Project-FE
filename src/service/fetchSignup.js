1cee7dedd0129d05f360d2a34bebb0655d909e22
Caleb Pendergraft <cackmed@gmail.com>
2020-02-26 11:33:07 -0800

    fixed services

--- src/service/fetchSignup.js
+++ src/service/fetchSignup.js
@@ -18,7 +18,7 @@ export const getUserSignup = ({
   dogImage
 }) => {
   return fetch(
-    'http://localhost:7890/api/v1/auth/signup',
+    'https://pet-project-be-staging.herokuapp.com/api/v1/auth/signup',
     {
       method: 'POST',
       credentials: 'include',
