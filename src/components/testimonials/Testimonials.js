801a8d45a5d30b3d64ec03b83eab13c35aaff01d
Nikki <nikki.satopdx@gmail.com>
2020-02-26 08:50:34 -0800

    saving changes

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ImageGallery from './ImageGallery';
-import './ImageGallery.scss';
 import './testimonials.scss';
 
 export default function Testimonials() {
@@ -12,21 +10,21 @@ export default function Testimonials() {
           <p>
             "I work nights, and most dog-walking services end at 6. I am so
             happy to know that Spot gets his chance to go potty"
-            <span className='username'>- Karen </span>
+            <span className='username'>Karen </span>
           </p>
         </div>
         <div className='speechbubble'>
           <p>
             "As a senior on a fixed income, I appreciate sharing with a
             neighbor. It is not just another service app"
-            <span className='username'>-Meryl</span>
+            <span className='username'>Meryl</span>
           </p>
         </div>
         <div className='speechbubble'>
           <p>
             "Pet pals isn't just a way to get your dog walked. It is a way to
             build connections."
-            <span className='username'> -Sam</span>
+            <span className='username'>Sam</span>
           </p>
         </div>
       </div>
