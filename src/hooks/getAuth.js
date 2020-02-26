d9e3a0bfa3daa08f59455f8e70e94f009c19931d
Zak Hyslin <rapscalan@gmail.com>
2020-02-26 11:25:45 -0800

    integrating fileUpload into getAuth

--- src/hooks/getAuth.js
+++ src/hooks/getAuth.js
@@ -2,7 +2,8 @@ import React, { createContext, useEffect, useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getUserLogin } from '../service/fetchLogin';
 import { fetchVerify } from '../service/fetchVerify';
-import { getUserSignup } from '../service/fetchSignup';
+import { fileUpload } from '../service/postImage';
+//import { getUserSignup } from '../service/fetchSignup';
 
 const SessionContext = createContext();
 
@@ -22,9 +23,10 @@ export const SessionProvider = ({ children }) => {
       });
   }, []);
   const signup = formData => {
-    getUserSignup(formData)
+    // getUserSignup(formData)
+    fileUpload(formData)
       .then(user => {
-        setUser(user)
+        setUser(user);
         history.push(`/zipcode/${user.address.zipcode}`);
       })
       .catch(err => {
