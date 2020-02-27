01f8a37ee41907d433d0435d93f05d79f198787b
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-27 10:30:25 -0800

    about started

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import './AboutUs.scss';
+import eve from '../assets/aboutUs/eveisla.png';
+import nikki from '../assets/aboutUs/nikki.png';
 
 const AboutUs = () => {
   return (
@@ -9,81 +10,74 @@ const AboutUs = () => {
       <ul className='AboutUl'>
         <article className='about'>
           <div className='img__container'>
-            
+            <img className='about__image' src={eve} alt='eve picture' />
           </div>
+          <h3 className='about__name'>Eve Stockstill</h3>
           <div className='about__info'>
-            I am a software developer who is obessed with her dog in my free
+            I am a software developer who is obsessed with her dog in my free
             time, I find new ways to spoil her, or you can find me around a pool
             table.
-            <h3 className='about__name'>Eve Stockstill</h3>
+            <li className='Li'>
+              <a href='https://www.github.com/evestockstill'>
+                https://www.github.com/evestockstill
+              </a>
+            </li>
+            <li className='Li'>
+              <a href='https://www.linkedin.com/in/evestockstill/'>
+                https://www.linkedin.com/in/evestockstill
+              </a>
+            </li>
+          </div>
+        </article>
+
+        <article className='about'>
+          <div className='img__container'>
+            <img className='about__image' src={nikki} alt='nikki picture' />
+          </div>
+          <h3 className='about__name'>Nikki Sato</h3>
+          <div className='about__info'>
+            a huge lover of Front End Design / Development! I loved combining my
+            creative side with a dash of coding. I&apos;m also a local portland
+            photography you can catch me wandering around portland catching
+            reflections.
+            <li className='Li'>https://www.linkedin.com/in/nikkisato/</li>
+            <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
+          </div>
+        </article>
+
+        <article className='about'>
+          <div className='img__container'>
+            <img className='about__image' src={eve} alt='eve picture' />
+          </div>
+          <h3 className='about__name'>Eve Stockstill</h3>
+          <div className='about__info'>
+            I am a software developer who is obsessed with her dog in my free
+            time, I find new ways to spoil her, or you can find me around a pool
+            table.
             <li className='Li'>https://github.com/evestockstill</li>
+            <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
+          </div>
+        </article>
+
+        <article className='about'>
+          <div className='img__container'>
+            <img className='about__image' src={eve} alt='eve picture' />
+          </div>
+          <h3 className='about__name'>Eve Stockstill</h3>
+          <div className='about__info'>
+            I am a software developer who is obsessed with her dog in my free
+            time, I find new ways to spoil her, or you can find me around a pool
+            table.
+            <li className='Li'>a href= 'https://github.com/evestockstill'</li>
             <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
           </div>
         </article>
         <article className='about'>
           <div className='img__container'>
-            <img
-              className='about__image'
-              src='../assets/'
-              alt='eve picture'
-            />
+            <img className='about__image' src={eve} alt='eve picture' />
           </div>
+          <h3 className='about__name'>Eve Stockstill</h3>
           <div className='about__info'>
-            I am a software developer who is obessed with her dog in my free
-            time, I find new ways to spoil her, or you can find me around a pool
-            table.
-            <h3 className='about__name'>Eve Stockstill</h3>
-            <li className='Li'>https://github.com/evestockstill</li>
-            <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
-          </div>
-        </article>
-        <article className='about'>
-          <div className='img__container'>
-            <img
-              className='about__image'
-              src='../assets/Isla3.png'
-              alt='eve picture'
-            />
-          </div>
-          <div className='about__info'>
-            I am a software developer who is obessed with her dog in my free
-            time, I find new ways to spoil her, or you can find me around a pool
-            table.
-            <h3 className='about__name'>Eve Stockstill</h3>
-            <li className='Li'>https://github.com/evestockstill</li>
-            <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
-          </div>
-        </article>
-        <article className='about'>
-          <div className='img__container'>
-            <img
-              className='about__image'
-              src='../assets/Isla3.png'
-              alt='eve picture'
-            />
-          </div>
-          <div className='about__info'>
-            I am a software developer who is obessed with her dog in my free
-            time, I find new ways to spoil her, or you can find me around a pool
-            table.
-            <h3 className='about__name'>Eve Stockstill</h3>
-            <li className='Li'>https://github.com/evestockstill</li>
-            <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
-          </div>
-        </article>
-        <article className='about'>
-          <div className='img__container'>
-            <img
-              className='about__image'
-              src='../assets/Isla3.png'
-              alt='eve picture'
-            />
-          </div>
-          <div className='about__info'>
-            I am a software developer who is obessed with her dog in my free
-            time, I find new ways to spoil her, or you can find me around a pool
-            table.
-            <h3 className='about__name'>Eve Stockstill</h3>
             <li className='Li'>https://github.com/evestockstill</li>
             <li className='Li'>https://www.linkedin.com/in/evestockstill/</li>
           </div>
@@ -111,7 +105,7 @@ const AboutUs = () => {
 //           photography you can catch me wandering around portland catching
 //           reflections.
 //         </p>
-//         <a href='https://www.linkedin.com/in/nikkisato/'>
+        // <a href='https://www.linkedin.com/in/nikkisato/'>
 //           <FaLinkedin size={32} />
 //         </a>
 //         <a href='https://github.com/nikkisato'>
@@ -173,6 +167,5 @@ const AboutUs = () => {
 //     </div>
 //   </>
 // );
-
 
 export default AboutUs;
