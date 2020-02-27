07216edbfac5d5e8e99d87b506793caf27d4528d
Nikki <nikki.satopdx@gmail.com>
2020-02-26 17:02:58 -0800

    deleted duplicated explorePage

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,7 @@ import Signup from './signup/Signup';
 import Login from './login/Login';
 import Home from './home/Home';
 import Nav from './nav/Nav';
-import ExplorePage from './testimonials/ExplorePage';
 import UserList from './user/UsersList';
-
 import PrivateRoute from '../components/private-public/PrivateRoute';
 import { SessionProvider } from '../hooks/getAuth';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
