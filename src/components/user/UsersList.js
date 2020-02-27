89b54cee7f88d2fa2cdac78e5f7fcc12e1def0d7
Zak Hyslin <rapscalan@gmail.com>
2020-02-27 15:33:44 -0800

    typos

--- src/components/user/UsersList.js
+++ src/components/user/UsersList.js
@@ -54,7 +54,7 @@ const UserList = ({ match }) => {
   });
   return (
     <>
-      <p className='otherUsers'>Hello, {user.firstName} The pet pals in {match.params.zipcode}</p>;
+      <p className='otherUsers'>Hello {user.firstName}, these are the Pet Pals in {match.params.zipcode}</p>;
       <ul className='ulUserList'>{userList}</ul>
     </>
   );
