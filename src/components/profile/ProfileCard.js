ad6add7c90d4bc059be90799eb6892856a5091b5
cackmed <53550131+cackmed@users.noreply.github.com>
2020-02-27 14:24:49 -0800

    profile displaying with information, context is cool (#33)
    * profile displaying with information, context is cool
    
    * Update Signup.scss
    
    Co-authored-by: Nikki Sato <54095031+nikkisato@users.noreply.github.com>

--- src/components/profile/ProfileCard.js
+++ src/components/profile/ProfileCard.js
@@ -12,46 +12,50 @@ const ProfileCard = ({
   timeAvailable,
   bio,
   dogBio
-}) => (
+}) => {
+  return (
+    <div className='card'>
+      <div className='card__side card__side--front'>
+        <div className='card__picture card__picture--1'>
+          <div className='card__picture card__img'>
+            <img className='userImg' src={userImg ? userImg : defaultUserImg} />
+            <figcaption>
+              <span className='card__heading-span card__heading-span--1'>
+                {firstName}&nbsp;&
+              </span>
+            </figcaption>
+            <section
+              className='dogImg'
+              style={{ backgroundImage: `url(${dogImg})` }}
+            />
+            <figcaption>
+              <span className='card__heading-span card__heading-span--2'>
+                {dogName}&nbsp;
+              </span>
+            </figcaption>
+          </div>
+        </div>
+        <div className='card__details'>
+          <ul>
+            <li>I can help {timeAvailable}</li>
+            <li>I need help {timeNeeded}</li>
+          </ul>
+        </div>
+      </div>
+      <div className='card__side card__side--back card__side--back-1'>
+        <div className='card__cta'>
+          <div className='card__bio-box'>
+            <p className='card__bio-text'>{bio}</p>
+            <p className='card__bio-text'>{dogBio}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+  
 
-  <div className='card'>
-    <div className='card__side card__side--front'>
-      <div className='card__picture card__picture--1'>
-        <div className='card__picture card__img'>
-          <img className='userImg' src={userImg ? userImg : defaultUserImg} />
-          <figcaption>
-            <span className='card__heading-span card__heading-span--1'>
-              {firstName}&nbsp;&
-            </span>
-          </figcaption>
-          <section
-            className='dogImg'
-            style={{ backgroundImage: `url(${dogImg})` }}
-          />
-          <figcaption>
-            <span className='card__heading-span card__heading-span--2'>
-              {dogName}&nbsp;
-            </span>
-          </figcaption>
-        </div>
-      </div>
-      <div className='card__details'>
-        <ul>
-          <li>I can help {timeAvailable}</li>
-          <li>I need help {timeNeeded}</li>
-        </ul>
-      </div>
-    </div>
-    <div className='card__side card__side--back card__side--back-1'>
-      <div className='card__cta'>
-        <div className='card__bio-box'>
-          <p className='card__bio-text'>{bio}</p>
-          <p className='card__bio-text'>{dogBio}</p>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+  
 
 ProfileCard.propTypes = {
   firstName: PropTypes.string.isRequired,
