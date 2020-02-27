bf424ad10e9211ff81d27f18839b50b4e28d280d
Caleb Pendergraft <cackmed@gmail.com>
2020-02-26 18:57:11 -0800

    I think it's working

--- src/service/postImage.js
+++ src/service/postImage.js
@@ -2,6 +2,11 @@
 export const fileUpload = (data) => {
   return fetch('https://pet-project-be-staging.herokuapp.com/api/v1/auth/signup', {
     method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      origin: true
+    },
     body: data
   })
     .then(res => res.json());
