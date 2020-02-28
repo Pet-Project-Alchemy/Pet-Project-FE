09d1465d78e5892d07ece05d6ccfdca5dbb0ba5f
samSterns <sam.g.sterns@gmail.com>
2020-02-27 20:42:29 -0800

    head left in merge

--- src/components/chat/HistoryList.js
+++ src/components/chat/HistoryList.js
@@ -5,23 +5,6 @@ import { getConversations } from '../../service/fetchMessages';
 export const HistoryList = () => {
   const [histories, setHistories] = useState([]);
   useEffect(() => {
-<<<<<<< HEAD
-    getConversations()
-      .then(conversations => {
-        setHistories(conversations);
-      });
-  }, []);
-  const historyArray = histories.map(history => {
-    
-    return <HistoryItem 
-      key={history._id}
-      history={history}
-    />;
-  });
-  return (
-    <ul>{historyArray}</ul>
-  );
-=======
     getConversations().then(conversations => {
       setHistories(conversations);
     });
@@ -30,5 +13,4 @@ export const HistoryList = () => {
     return <HistoryItem key={history._id} history={history} />;
   });
   return <ul className='historyUl'>{historyArray}</ul>;
->>>>>>> 9ca732c1f894443ba762ab2fee09bb42e2819cc7
 };
