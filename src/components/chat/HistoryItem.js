bf7b398b4b3b7ae997619884934b60b30aaca941
Nikki <nikki.satopdx@gmail.com>
2020-02-28 09:55:40 -0800

    testing

--- src/components/chat/HistoryItem.js
+++ src/components/chat/HistoryItem.js
@@ -3,10 +3,14 @@ import './HistoryItem.scss';
 import { Link } from 'react-router-dom';
 const HistoryItem = ({ history }) => {
   return (
-    <li className='historyLinksLi'>
-    <li>
-      <Link to={`/chat/${history._id}`} className='historyLink'>{history.userName}</Link>
-    </li>
+    <>
+      <li className='historyLinksLi' />
+      <li>
+        <Link to={`/chat/${history._id}`} className='historyLink'>
+          {history.userName}
+        </Link>
+      </li>
+    </>
   );
 };
 export default HistoryItem;
