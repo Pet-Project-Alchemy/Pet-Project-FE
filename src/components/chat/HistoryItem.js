ccd6a6e253e111de208f9c278a80c37c630915ae
Eve Stockstill <51726275+evestockstill@users.noreply.github.com>
2020-02-28 09:32:50 -0800

    Message styles (#49)
    * stashing
    
    * chat
    
    * cssFixes
    
    * navfix
    
    Co-authored-by: samSterns <54450022+samSterns@users.noreply.github.com>

--- src/components/chat/HistoryItem.js
+++ src/components/chat/HistoryItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './HistoryItem.scss';
 import { Link } from 'react-router-dom';
 const HistoryItem = ({ history }) => {
   return (
+    <li className='historyLinksLi'>
     <li>
       <Link to={`/chat/${history._id}`} className='historyLink'>{history.userName}</Link>
     </li>
