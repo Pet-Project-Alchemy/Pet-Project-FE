317642ed2d611d237bfe3e8df00900dcadc759c3
samSterns <sam.g.sterns@gmail.com>
2020-02-27 20:46:27 -0800

    fux merge conflict in vscode

--- src/components/chat/HistoryItem.js
+++ src/components/chat/HistoryItem.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-
-const HistoryItem = ({ history }) => {
-  return (
-    <li>
-      <Link to={`/chat/${history._id}`}>{history.userName}</Link>
-=======
 const HistoryItem = ({ history }) => {
   return (
     <li>
       <Link to={`/chat/${history._id}`} className='historyLink'>{history.userName}</Link>
->>>>>>> 9ca732c1f894443ba762ab2fee09bb42e2819cc7
     </li>
   );
 };
