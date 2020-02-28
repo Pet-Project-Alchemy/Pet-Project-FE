b111791ac2f596fa3091349dd5307cb34d8c3042
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-28 09:44:01 -0800

    label fix

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -150,10 +150,10 @@ export default function UserSignup() {
                     value={street}
                     name='street'
                     onChange={handleChange}
-                    placeholder='Street Address (only zipcode displayed)'
+                    placeholder='Street Address (zipcode displayed)'
                   />
                   <label htmlFor='street' className='form__label'>
-                    Street Address (We only display your zipcode)
+                    Street Address 
                   </label>
                 </div>
                 <div className='form__group'>
@@ -198,7 +198,7 @@ export default function UserSignup() {
                     value={dogName}
                     name='dogName'
                     onChange={handleChange}
-                    placeholder='spot'
+                    placeholder='Spot'
                   />
                   <label htmlFor='dogName' className='form__label'>
                     Dogs name
