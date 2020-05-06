ae3fa75321d9b685c3191243e91baa1b02139c65
Eve Stockstill <evestockstillpdx@gmail.com>
2020-05-06 00:38:09 -0700

    fixed signup mobile color and brought back card

--- src/components/profile/ProfileCard.js
+++ src/components/profile/ProfileCard.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import UserCard from '../user/UserCard';
+import './ProfileCard.scss';
+import defaultUserImg from '../assets/dogPhoto/profile.png';
+import defaultDogImage from '../assets/dogPhoto/dog.png';
 
-const ProfileCard = () => {
+const ProfileCard = ({
+  firstName,
+  dogName,
+  userImg,
+  dogImg,
+  timeNeeded,
+  timeAvailable,
+  bio,
+  dogBio,
+}) => {
   return (
-    <div className='profileCard'>
-      <UserCard />
-    </div>
+    <main className='card-wrapper'>
+      <div className='ProfileCard'>
+        <div className='ProfileCard__side ProfileCard__side--front'>
+          <div className='ProfileCard__picture ProfileCard__picture--1'>
+            <div className='ProfileCard__picture ProfileCard__img'>
+              <img
+                className='userImg'
+                src={userImg ? userImg : defaultUserImg}
+              />
+              <figcaption>
+                <span className='ProfileCard__heading-span ProfileCard__heading-span--1'>
+                  {firstName}
+                </span>
+              </figcaption>
+              <img className='dogImg' src={dogImg ? dogImg : defaultDogImage} />
+              <figcaption>
+                <span className='ProfileCard__heading-span ProfileCard__heading-span--2'>
+                  {dogName}&nbsp;
+                </span>
+              </figcaption>
+            </div>
+          </div>
+          <div className='ProfileCard__details'>
+            <ul>
+              <li>I can help {timeAvailable}</li>
+              <li>I need help {timeNeeded}</li>
+            </ul>
+          </div>
+        </div>
+        <div className='ProfileCard__side ProfileCard__side--back ProfileCard__side--back-1'>
+          <div className='ProfileCard__cta'>
+            <div className='ProfileCard__bio-box'>
+              <p className='ProfileCard__bio-text'>{bio}</p>
+              <p className='ProfileCard__bio-text'>{dogBio}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
+
 ProfileCard.propTypes = {
   firstName: PropTypes.string.isRequired,
   dogName: PropTypes.string.isRequired,
